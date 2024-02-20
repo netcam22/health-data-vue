18fe09b633a6d28bea23c53ed9a1254d661fafb5
netcam22 <annettelesage@btinternet.com>
2024-02-20 08:24:45 +0000

    refactor: update to use computed properties so chart updates when dependencies change

--- src/components/DataChart.vue
+++ src/components/DataChart.vue
@@ -1,16 +1,21 @@
 <script setup>
+import {computed} from 'vue';
 import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
-const chartData = {
+const chartData = computed(() => {
+    return {
         labels: [ 'January', 'February', 'March', 'April' ],
         datasets: [ { data: [40, 20, 12, 10] } ]
-};
-const chartOptions = {
+    }
+});
+const chartOptions = computed(() => {
+    return {
         responsive: true
-}
+    }
+});
 </script>
 
 <template>
