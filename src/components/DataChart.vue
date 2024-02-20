d83b38cd75fcddd24f2e7e16ff7fee79bd41c620
netcam22 <annettelesage@btinternet.com>
2024-02-20 08:14:35 +0000

    refactor: chart data to use script setup syntax

--- src/components/DataChart.vue
+++ src/components/DataChart.vue
@@ -1,23 +1,15 @@
-<script>
+<script setup>
 import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
-export default {
-    name: 'BarChart',
-    components: { Bar },
-    data() {
-        return {
-        chartData: {
-            labels: [ 'January', 'February', 'March' ],
-            datasets: [ { data: [40, 20, 12] } ]
-        },
-        chartOptions: {
-            responsive: true
-        }
-        }
-    }
+const chartData = {
+        labels: [ 'January', 'February', 'March', 'April' ],
+        datasets: [ { data: [40, 20, 12, 10] } ]
+};
+const chartOptions = {
+        responsive: true
 }
 </script>
 
