7845911ec7bf160cbf97deea62bcf372680e9274
netcam22 <annettelesage@btinternet.com>
2024-02-21 13:41:16 +0000

    feat: implement styling for call items

--- src/components/CallItem.vue
+++ src/components/CallItem.vue
@@ -6,19 +6,31 @@ defineProps({
 </script>
 
 <template>
-  <div class="item">
-      <h2 class="heading">{{ title }}</h2>
-      <p class = "text" v-for="(text, index) in content" :key="index" >
+      <h2 class="heading">{{ title }}:</h2>
+      <div class = "call-item">
+      <p class = "call-item__text" v-for="(text, index) in content" :key="index" >
       {{ text }}
     </p>
   </div>
 </template>
 
 <style lang="scss" scoped>
+
 .heading {
   color: $dark-text;
+  margin: 5px 10px 0 20px;
+  padding: 0;
+  font-size: 25px;
+  margin-top: 5px;
+  }
+.call-item {
+  &__text {
+    color: $dark-text;
+    margin: 0 10px 0 20px;
+    padding: 0;
+    line-height: 1.2;
+    font-size: 20px;
+  }
 }
-.text {
-  color: $dark-text;
-}
+
 </style>
